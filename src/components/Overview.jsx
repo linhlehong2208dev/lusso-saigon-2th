044ed91ea85e1e92b5d9a75506c26ee2c2990dad
Le Hong Linh <linhlh@rever.vn>
2026-03-30 11:51:11 +0700

    fix

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -9,8 +9,8 @@ export default function Overview() {
     <div className={styles.wrapper} id="tong-quan">
       <div className={styles.container}>
         {/* TITLE */}
-        <h1 className={styles.title}>TỔNG QUAN DỰ ÁN LUSSO SAIGON</h1>
-
+        <h1 className={styles.title}>TỔNG QUAN DỰ ÁN</h1>
+        <h1 className={styles.title}>LUSSO SAIGON</h1>
         {/* GRID */}
         <div className={styles.grid}>
           {/* Card 1 */}
